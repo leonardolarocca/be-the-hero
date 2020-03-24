77f38c888413fc7d75c7023c5795a16f6375f149
Leonardo Larocca <leonardo_larocca@hotmail.com>
2020-03-23 22:08:27 -0300

    Configurando ambiente de dev

---
+++ api/src/api.ts
@@ -0,0 +1,21 @@
+class Api {
+  constructor () {
+    this.database()
+    this.middleware()
+    this.routes()
+  }
+
+  private database (): void {
+    //
+  }
+
+  private middleware (): void {
+    //
+  }
+
+  public routes (): void {
+    //
+  }
+}
+
+export default new Api()
